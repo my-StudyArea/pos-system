0af830a1e9ad52f45ac03607f6e7abaf59d6b3cb
Dasun Tharaka <wkdasunt0001@gmail.com>
2023-04-23 12:59:57 +0530

    Commit-01

--- src/main/java/ik/ijse/dep10/pos/controller/AdminLoginController.java
+++ src/main/java/ik/ijse/dep10/pos/controller/AdminLoginController.java
@@ -1,4 +1,36 @@
 package ik.ijse.dep10.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class AdminLoginController {
+
+    @FXML
+    private Button btnCreatAccount;
+
+    @FXML
+    private Label topic;
+
+    @FXML
+    private TextField txtFullName;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    void btnCreatAccountOnAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/ik/ijse/dep10/pos/controller/LoginPageController.java
+++ src/main/java/ik/ijse/dep10/pos/controller/LoginPageController.java
@@ -1,4 +1,31 @@
 package ik.ijse.dep10.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class LoginPageController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
 }
+
+
